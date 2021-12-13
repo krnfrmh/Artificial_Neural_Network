18e6a33cb016724dd19b2bcd4a53aa0260043db5
krnfrmh <87569650+krnfrmh@users.noreply.github.com>
2021-12-13 23:57:55 +0530

    Random weight initialization

--- forward_prop.py
+++ forward_prop.py
@@ -10,3 +10,15 @@ def get_data(Nclass):
   X = np.vstack([X1, X2, X3])
   Y = np.array([0]*Nclass + [1]*Nclass + [2]*Nclass)
   return X, Y
+
+X, Y = get_data(Nclass)
+
+D = 2 # Input dimensionality
+M = 3 # Hidden layer size
+K = 3 # Number of classes
+
+# randomly initialize weights
+W1 = np.random.randn(D, M)
+b1 = np.random.randn(M)
+W2 = np.random.randn(M, K)
+b2 = np.random.randn(K)
