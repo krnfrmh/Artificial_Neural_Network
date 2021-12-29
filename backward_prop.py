3903567df74dfb673f083e8e967d716db26e88a2
krnfrmh <87569650+krnfrmh@users.noreply.github.com>
2021-12-29 23:27:28 +0530

    Forward pass through single hidden layer Neural Network

--- backward_prop.py
+++ backward_prop.py
@@ -1,2 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+def forward(X, W1, b1, W2, b2):
+    logit = X.dot(W1) + b1
+    # 1st Hidden Layer
+    Z = 1 / (1 + np.exp(-1 * logit))
+    # Output Layer
+    A = Z.dot(W2) + b2
