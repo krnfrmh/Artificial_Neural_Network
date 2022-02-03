b6b97c31ecde40838f3319300290199edf51e5aa
krnfrmh <87569650+krnfrmh@users.noreply.github.com>
2022-02-03 23:11:11 +0530

    Generate sample data

--- backward_prop.py
+++ backward_prop.py
@@ -48,4 +48,11 @@ def cost(T, Y):
     tot = T * np.log(Y)
     return tot.sum()
     
+def generate_data(Nclass, D): 
+    X1 = np.random.randn(Nclass, D) + np.array([0, -2])
+    X2 = np.random.randn(Nclass, D) + np.array([2, 2])
+    X3 = np.random.randn(Nclass, D) + np.array([-2, 2])
     
+    X = np.vstack([X1, X2, X3])
+    Y = np.array([0]*Nclass + [1]*Nclass + [2]*Nclass)
+    return X, Y
